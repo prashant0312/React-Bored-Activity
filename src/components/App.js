9d9b4c104acd479e2bfb215781544b1922b77b18
Prashant Madhukar Kale <61052188+prashant0312@users.noreply.github.com>
2022-10-03 18:35:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import '../styles/App.css';
 
-const Loader = () => <div id="loader">Loading...</div>
-
+const Loader = () => {
+  <div id="loader">Loading...</div>;
+};
 
 const App = () => {
+  const [getdata, setData] = useState("");
+  const [getloading, setLoading] = useState(true);
 
-  
+  const makeURL = async (type) => {
+    const getdata = await fetch(
+      `https://www.boredapi.com/api/activity?type=${type}`
+    );
+    const response = await getdata.json();
+    setLoading(false);
+    setData(response.activity);
+  };
 
+  useEffect(() => {
+    makeURL("education");
+  }, []);
+
+  const handleR = () => {
+    setLoading(true);
+    makeURL("recreational");
+  };
+  const handleE = () => {
+    setLoading(true);
+    makeURL("education");
+  };
   return (
-    <div id="main">
-    
+    <div id="activity">
+      { getloading ? Loader() : getdata }
+      <br />
+      <button id="btn-recreation" onClick={handleR}>
+        Recreational
+      </button>
+      <button id="btn-education" onClick={handleE}>
+        Education
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
