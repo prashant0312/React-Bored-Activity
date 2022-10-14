4250cd67a2df6d2528c2da7ad807dd3b67cbb54a
Prashant Madhukar Kale <61052188+prashant0312@users.noreply.github.com>
2022-10-14 19:05:38 +0530

    pk0312

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,39 @@
 import React, { useState, useEffect } from "react";
-import "./styles.css";
+import "../styles/App.css";
 
 const Loader = () => <div id="loader">Loading...</div>;
 
 const App = () => {
-  const [activity, setActivity] = useState("");
-  const [type, setType] = useState("education");
-  const [loading, setLoading] = useState(false);
-
-  async function fetching() {
-    setLoading(true);
-    const data = await fetch(
-      `https://www.boredapi.com/api/activity?type=${type}`
-    );
-    const res = await data.json();
-    setActivity(res.activity);
-    setLoading(false);
+  // function for url link
+  function makeURL(type) {
+    console.log(type);
+    return `https://www.boredapi.com/api/activity?type=${type}`;
   }
-  useEffect(() => {
-    fetching();
-  }, [type]);
+  // We can use two types
+  // recreational or education.
+  //
+  const [activity, setActivity] = useState(null);
+
+  async function getResponse(z) {
+    setActivity(null);
+
+    let a = makeURL(z);
+
+    const response = await fetch(a);
+    let data = await response.json();
+    setActivity(data.activity);
+  }
+
+  useEffect(() => getResponse("education"), []);
 
   return (
     <div id="main">
-      {loading ? <Loader /> : <div id="activity">{activity}</div>}
-      <button id="btn-recreation" onClick={() => setType("recreational")}>
-        recreational
+      {activity ? <div id="activity"> {activity}</div> : <Loader />}
+      <button id="btn-recreation" onClick={() => getResponse("recreational")}>
+        Recreational
       </button>
-      <button id="btn-education" onClick={() => setType("education")}>
-        education
+      <button id="btn-education" onClick={() => getResponse("education")}>
+        Education
       </button>
     </div>
   );
