b75db59bcbd153c5e7fe03f730ba7698977734ce
Prashant Madhukar Kale <61052188+prashant0312@users.noreply.github.com>
2022-11-04 16:59:18 +0530

    pk0312

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,36 @@
-import React, { useState, useEffect } from "react";
-import "./styles.css";
+import React ,{useState,useEffect}from 'react'
+import '../styles/App.css';
 
-const Loader = () => <div id="loader">Loading...</div>;
+const Loader = () => <div id="loader">Loading...</div>
+
 
 const App = () => {
-  const [activity, setActivity] = useState("");
-  const [type, setType] = useState("education");
-  const [loading, setLoading] = useState(false);
 
-  async function fetching() {
+  let [activity,setActivity] = useState('');
+  let [type,setType] = useState('education');
+  let [loading,setLoading] = useState(false);
+
+  async function fetching(){
     setLoading(true);
-    const data = await fetch(
-      `https://www.boredapi.com/api/activity?type=${type}`
-    );
+    const data = await fetch(`http://www.boredapi.com/api/activity?type=${type}`)
     const res = await data.json();
     setActivity(res.activity);
-    setLoading(false);
+    setLoading(false)
   }
-  useEffect(() => {
-    fetching();
-  }, [type]);
+
+  useEffect(()=>{
+   fetching();
+  },[type])
 
   return (
+
     <div id="main">
-      {loading ? <Loader /> : <div id="activity">{activity}</div>}
-      <button id="btn-recreation" onClick={() => setType("recreational")}>
-        recreational
-      </button>
-      <button id="btn-education" onClick={() => setType("education")}>
-        education
-      </button>
+        {loading ? (<Loader />) : (<div id="activity">{activity}</div>)}
+        <button id="btn-recreation" onClick={()=>setType('recreational')}>recreational</button>
+        <button id="btn-education" onClick={()=>setType('education')}>education</button>
     </div>
-  );
-};
+  )
+}
+
 
 export default App;
